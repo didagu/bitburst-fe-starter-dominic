96c89430d31ec57a3d4b12be702f89063d36c979
Dominic Idagu <dominic.idagu@abas.de>
2023-12-18 01:35:26 +0100

    Add backlog list

--- src/views/BacklogList.vue
+++ src/views/BacklogList.vue
@@ -1,3 +1,26 @@
 <template>
-  <div>Backlog</div>
+  <div class="backlog-list">
+    <ul>
+      <li v-for="item in itemsStore.backlog" :key="item.id">
+        <div class="todo-list-item">
+          <TodoItem :todo="item" type="backlog"/>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
+<script setup lang="ts">
+import TodoItem from "@/components/TodoItem.vue";
+import { useItemsStore } from '@/store/items';
+
+const itemsStore = useItemsStore();
+
+</script>
+<style scoped>
+.todo-list-item {
+  @apply p-8;
+}
+.backlog-list {
+  @apply pt-8
+}
+</style>
