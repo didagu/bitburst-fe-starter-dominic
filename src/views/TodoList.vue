2e39eb5501330a261be889d58f018edaaf32f52b
Dominic Idagu <dominic.idagu@abas.de>
2023-12-18 01:29:26 +0100

    Add TodoItem to TodoList

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>TodoList</div>
+  <div class="todo-list">
+    <ul>
+      <li v-for="item in itemsStore.items" :key="item.id">
+        <TodoItem class="todo-list-item" :todo="item" type="uncompleted"/>
+      </li>
+    </ul>
+  </div>
 </template>
+<script setup lang="ts">
+import TodoItem from "@/components/TodoItem.vue";
+import { useItemsStore } from '@/store/items';
+
+const itemsStore = useItemsStore();
+</script>
+<style scoped>
+.todo-list-item {
+  @apply p-8;
+}
+
+</style>
