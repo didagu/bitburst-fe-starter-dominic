13674d3715b5bc88ed50ef9bf642bfc989f2ee4e
Dominic Idagu <dominic.idagu@abas.de>
2023-12-18 01:18:28 +0100

    Add seed data for store

---
+++ src/fixtures/seeds.ts
@@ -0,0 +1,140 @@
+export const list = [
+  {
+    id: '1',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonu',
+    createdAt: "31/12/1978, 07:22 pm",
+    completed: false,
+  },
+  {
+    id: '2',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonu',
+    createdAt: "31/12/1978, 07:23 pm",
+    completed: false,
+  },
+  {
+    id: '3',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonu',
+    createdAt: "31/12/1978, 07:24 pm",
+    completed: false,
+  },
+  {
+    id: '4',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonu',
+    createdAt: "31/12/1978, 07:25 pm",
+    completed: false,
+  },
+  {
+    id: '5',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonu',
+    createdAt: "31/12/1978, 07:26 pm",
+    completed: false,
+  },
+  {
+    id: '6',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonu',
+    createdAt: "31/12/1978, 07:27 pm",
+    completed: false,
+  },
+  {
+    id: '7',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonu',
+    createdAt: "31/12/1978, 07:28 pm",
+    completed: false,
+  },
+  {
+    id: '8',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonu',
+    createdAt: "31/12/1978, 07:29 pm",
+    completed: false,
+  },
+  {
+    id: '9',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonu',
+    createdAt: "31/12/1978, 07:30 pm",
+    completed: false,
+  },
+  {
+    id: '10',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonu',
+    createdAt: "31/12/1978, 07:31 pm",
+    completed: false,
+  },
+  {
+    id: '12',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonu',
+    createdAt: "31/12/2043, 07:32 pm",
+    completed: false,
+  },
+  {
+    id: '13',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonu',
+    createdAt: "31/12/2054, 07:33 pm",
+    completed: false,
+  },
+  {
+    id: '14',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonu',
+    createdAt: "31/12/1999, 07:34 pm",
+    completed: false,
+  },
+  {
+    id: '15',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonu',
+    createdAt: "31/12/1978, 07:35 pm",
+    completed: false,
+  },
+  {
+    id: '31',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonu',
+    createdAt: "31/12/1978, 07:36 pm",
+    completed: false,
+  }
+]
+
+export const backlog = [
+  {
+    id: '18',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonu',
+    createdAt: "01/12/2023, 07:37 pm",
+    completed: false,
+  },
+  {
+    id: '17',
+    title: 'Lorem ipmpor invidunt ut labore et dolore magna aliquyam erat nonu',
+    createdAt: "31/12/1978, 07:45 pm",
+    completed: false,
+  },
+  {
+    id: '21',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonu',
+    createdAt: "09/12/2023, 07:46 pm",
+    completed: false,
+  },
+  {
+    id: '19',
+    title: 'Stet clita kasd gubergren, no sea takimata ',
+    createdAt: "31/12/1978, 07:24 am",
+    completed: false,
+  },
+  {
+    id: '20',
+    title: 't accusam et justo duo dolores et ea',
+    createdAt: "26/12/2023, 08:24 pm",
+    completed: false,
+  }
+]
+
+export const completedItems = [
+  {
+    id: '23',
+    title: 'random text 23',
+    createdAt: "06/12/2023, 09:24 pm",
+    completed: true,
+  },
+  {
+    id: '22',
+    title: 'random text 22',
+    createdAt: "31/12/1978, 07:59 am",
+    completed: true,
+  }
+]
